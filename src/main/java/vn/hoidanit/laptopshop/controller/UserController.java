0d423f1fb3aee8f5b1a190f991bdb8b8711408aa
NguyenMinhCao <caonguyenlaptrinh@gmail.com>
2024-08-20 21:08:02 +0700

    update setup jsp

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,37 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "hello";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
--- src/main/java/vn/hoidanit/laptopshop/hihi.java
+++
@@ -1,5 +0,0 @@
-package vn.hoidanit.laptopshop;
-
-public class hihi {
-    
-}
