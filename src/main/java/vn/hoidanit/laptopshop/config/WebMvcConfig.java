2a00f98a07cdcc851f843b6a3dbc5ea7db0b6986
NguyenMinhCao <caonguyenlaptrinh@gmail.com>
2024-08-25 03:21:49 +0700

    dashboard

--- src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
+++ src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
@@ -30,5 +30,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
     }
+
 }
